0744f3398013097274d39a667acbd3e83908278a
foxhebert <foxhebert@gmail.com>
2022-06-03 18:45:29 -0500

    Subida de Nuevo Proyecto  03.06.2022

---
+++ SisGuauMiauWeb/Models/LoginRestfulClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//------------------------------------ Consumir Servicios
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace SisGuauMiauWeb.Models
+{
+    public class LoginRestfulClient
+    {
+
+        private string BASE_URL = "http://localhost:7070/apiProcesos/gmValidarUsuario";
+
+        public Task<string> usuarioValidacion(String usu, String psw)
+        {
+
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
+                //HttpResponseMessage response = client.GetAsync(String.Format("validarUsuario")).Result;
+                HttpResponseMessage response = client.GetAsync(String.Format("gmValidarUsuario/{0}/{1}", usu, psw)).Result;
+                //HttpResponseMessage response = client.GetAsync(String.Format($"/gmValidarUsuario/{usu}")).Result;
+                return response.Content.ReadAsStringAsync();
+
+                //api/products/id={0}&type={1}",param.Id.Value,param.Id.Type
+            }
+            catch(Exception ex)
+            {
+                String msgex = ex.Message;
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
+
+        }
+
+    }
+}
